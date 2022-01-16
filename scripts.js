e8b75e77f8a020a7468fb398081226b73997be3d
FSOsuit <druzhba2@inbox.lv>
2022-01-16 17:35:58 +0200

    made sizing of grids more dynamic

--- scripts.js
+++ scripts.js
@@ -6,9 +6,11 @@ for (let i = 1; i <= 16; i++) {
     const grid = document.createElement('div');
     grid.classList.add('grid');
     grid.textContent = j;
+    let widthOfGrid = 100 / 16;
+    let heightOfGrid = 100 / 16; 
+    grid.setAttribute('style', `width: ${widthOfGrid}%; height: ${heightOfGrid}%`);
     container.appendChild(grid);
   }
-  
 }
 
 //chages color of each moused over div
