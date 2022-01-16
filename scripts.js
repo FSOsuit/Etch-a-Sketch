785b3d56653df267287aa98c07ef4f8a6598f3ff
FSOsuit <druzhba2@inbox.lv>
2022-01-16 14:53:54 +0200

    created grid with hard coded values

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 const container = document.querySelector('.container');
 
-for (let i = 1; i < 17; i++) {
-  const grid = document.createElement('div');
-  grid.classList.add('grid');
-  grid.textContent = i;
-  container.appendChild(grid);
+for (let i = 1; i <= 16; i++) {
+  for (let j = 1; j <= 16; j++) {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    grid.textContent = j;
+    container.appendChild(grid);
+  }
+  
 }
